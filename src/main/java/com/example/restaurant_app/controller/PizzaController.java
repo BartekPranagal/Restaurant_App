4adc6a361fd172059a7e0314eefe03180051083f
agorny112 <agorny112@gmail.com>
2022-01-09 18:26:16 +0100

    Additional endpoints

--- src/main/java/com/example/restaurant_app/controller/PizzaController.java
+++ src/main/java/com/example/restaurant_app/controller/PizzaController.java
@@ -1,5 +1,6 @@
 package com.example.restaurant_app.controller;
 
+import com.example.restaurant_app.model.dto.PizzaRequest;
 import com.example.restaurant_app.model.dto.PizzaResponse;
 import com.example.restaurant_app.service.PizzaService;
 import lombok.RequiredArgsConstructor;
@@ -28,5 +29,22 @@ public class PizzaController {
     public List<PizzaResponse> getDynamicPizzaName(@RequestParam(required = false) String query){
         return pizzaService.getPizzaByQuery(query);
     }
+
+    @DeleteMapping(path = "menu/{id}")
+    public Long deletePizza(@PathVariable(name = "id") Long pizzaId){
+        return pizzaService.deletePizza(pizzaId);
+    }
+
+    @PostMapping(path = "/menu")
+    public PizzaResponse addNewPizza(@RequestBody PizzaRequest pizzaRequest){
+        return pizzaService.createNewPizza(pizzaRequest);
+    }
+
+    @PutMapping(path = "/{id}")
+    public PizzaResponse modifyPizza(@PathVariable(name = "id") Long pizzaId,@RequestBody PizzaRequest pizzaRequest){
+        return pizzaService.updatePizza(pizzaId,pizzaRequest);
+    }
+
+
 
 }
