d00fca701f5cc795806b44384975cfafc7fa77c5
BartekPranagal <bpranagal@gmail.com>
2022-01-16 13:57:11 +0100

    Dodanie do newUserRequest wymaganych pól

--- src/main/java/com/example/restaurant_app/model/dto/NewUserRequest.java
+++ src/main/java/com/example/restaurant_app/model/dto/NewUserRequest.java
@@ -1,6 +1,7 @@
 package com.example.restaurant_app.model.dto;
 
 import lombok.Data;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import javax.validation.constraints.NotEmpty;
 
@@ -11,4 +12,30 @@ public class NewUserRequest {
 
     @NotEmpty
     private String password;
+
+    @NotEmpty
+    private String first_name;
+
+    @NotEmpty
+    private String last_name;
+
+    @NotEmpty
+    private  String username;
+
+    @NotEmpty
+    private String street;
+
+    @NotEmpty
+    private Integer street_number;
+
+    @NotEmpty
+    private String postal_code;
+
+    @NotEmpty
+    private Integer phoneNumber;
+
+
+
+
 }
+// mail, haslo, nazwa uzytkownika, imie nazwisko, adres.
